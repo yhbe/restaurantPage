be6decf3afdd6b467b3cc9d66834676b636f6109
SHEF <Shefqetmush@gmail.com>
2022-10-17 09:23:23 -0400

    added classList and styling

--- src/menu.js
+++ src/menu.js
@@ -3,10 +3,11 @@ export function menu() {
   content.innerHTML = "";
   //remove children
   const h1 = document.createElement("h1");
+  h1.classList.add("landingTitle");
   h1.innerHTML = "Our extended menu ~";
   content.appendChild(h1);
   const p = document.createElement("p");
-  p.classList.add("menuP");
+  p.classList.add("landingP");
   p.innerHTML = "Potato Salad $5 <br> Hamburger $3 <br> Pizza $2";
   content.appendChild(p);
 }
